9d261bbdd4ce4a8e229ead1f541985c467f1010a
mordasuabae <alaricomanjate21@gmail.com>
2023-04-09 21:00:41 +0200

    my sencond session on my to do list

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
-var taskForm = document.querySelector('.taskForm')
-console.log(taskForm)
+var taskForm;
+var addButton;
+var cancelButton;
+var saveButton;
 
-var myButton = document.querySelector('.buttontwo')
-myButton.addEventListener('click', showModal);
+/**
+ * startApp will select all the element and make them 
+ * globally available accross the app, and 
+ * add event listeners to the elements
+ * 
+ * @param {*} params 
+ */
+function startApp(params) {
+     taskForm = document.querySelector('.taskForm')
+     addButton = document.querySelector('.add-button')
+     cancelButton = document.querySelector('.cancelButton')
+     saveButton = document.querySelector('.saveButton')
+     // form = document.querySelector('.form')
 
-var cancelButton = document.querySelector('.cancelButton')
-var saveButton = document.querySelector('.saveButton')
+     console.log(taskForm, addButton, cancelButton, cancelButton)
+    addButton.addEventListener('click', showModal);
+    cancelButton.addEventListener('click', hideModal);
+    saveButton.addEventListener('click', saveTask);
+   // form.addEventListener('click', value)
 
-cancelButton.addEventListener('click', hideModal);
-saveButton.addEventListener('click', hideModal);
+}
 
 
-function showModal() {
+function showModal(event) {
+    event.preventDefault()
     console.log('Hey there clicker!');
    // taskForm.style.display= 'block'
 
@@ -20,12 +36,60 @@ function showModal() {
 }
 
 
-function hideModal() {
-    console.log('Hey there clicker!');
+function hideModal(event) {
+    event.preventDefault()
+    console.log(' cancel button has been clicked');
    //taskForm.style.display= 'none'
 
    taskForm.classList.add('hide')
    taskForm.classList.remove('show')
 }
 
+function saveTask(event){
+    event.preventDefault()
+    console.log(' save button has been clicked');
 
+    const nameInput = document.querySelector('#name')
+    const statusInput = document.querySelector('#status')
+    const dateInput = document.querySelector('#date')
+
+    console.log(nameInput.value,statusInput.value,dateInput.value)
+
+    const taskList = document.querySelector('.task-list')
+
+    const container = document.createElement('div')
+    container.classList.add('task-container')
+
+
+    const taskNameSpan = document.createElement('span')
+    taskNameSpan.innerHTML = nameInput.value
+    const statusSpan = document.createElement('span')
+    statusSpan.innerHTML = statusInput.value
+    const dateSpan = document.createElement('span')
+    dateSpan.innerHTML = dateInput.value
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'Delete'
+    const editButton = document.createElement('button')
+    editButton.innerHTML = 'Edit'
+
+    container.append([taskNameSpan, statusSpan, dateSpan, editButton, deleteButton])
+    taskList.appendChild(container)
+
+}
+
+function cancelButton(event){
+    event.preventDefault()
+    console.log(' cancel button has been clicked');
+
+    
+
+}
+startApp()
+// Getting value from input elements 
+
+function value(event) {
+    event.preventDefault()
+    var input = document.getElementById("name");
+    console.log(input.value)
+  }
